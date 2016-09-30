e9871307ee9f686cad3c6af0afd02d3f136c8e83
Ali Abbas Rizvi <aliabbasrizvi@users.noreply.github.com>
2016-09-30 11:44:43 -0700

    Handle exceptions in event dispatcher (#30)

--- optimizely/event_dispatcher.py
+++ optimizely/event_dispatcher.py
@@ -1,5 +1,8 @@
 import json
+import logging
 import requests
+
+from requests import exceptions as request_exception
 
 from .helpers import enums
 
@@ -15,7 +18,11 @@ class EventDispatcher(object):
     Args:
       event: Object holding information about the request to be dispatched to the Optimizely backend.
     """
-    if event.http_verb == enums.HTTPVerbs.GET:
-      requests.get(event.url, params=event.params, timeout=REQUEST_TIMEOUT)
-    elif event.http_verb == enums.HTTPVerbs.POST:
-      requests.post(event.url, data=json.dumps(event.params), headers=event.headers, timeout=REQUEST_TIMEOUT)
+
+    try:
+      if event.http_verb == enums.HTTPVerbs.GET:
+        requests.get(event.url, params=event.params, timeout=REQUEST_TIMEOUT)
+      elif event.http_verb == enums.HTTPVerbs.POST:
+        requests.post(event.url, data=json.dumps(event.params), headers=event.headers, timeout=REQUEST_TIMEOUT)
+    except request_exception.RequestException as error:
+      logging.error('Dispatch event failed. Error: %s' % str(error))
