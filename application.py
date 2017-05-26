282c03c8e79cdfa7bd26cf3dad5bdf0cad6f173f
Andrew Delikat <andrewdelikat@gmail.com>
2017-05-26 16:40:11 -0700

    Add support for user profiles (#1)
    * Add support for user profiles
    
    * Respond to Mike's feedback

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask import request
 from optimizely import optimizely
 from optimizely import logger
+import user_profile_service
 
 app = Flask(__name__)
 
@@ -10,7 +11,25 @@ datafile = open('datafile.json', 'r')
 datafile_content = datafile.read()
 datafile.close()
 
-opt_obj = optimizely.Optimizely(datafile_content, logger=logger.SimpleLogger())
+optimizely_instance = None
+user_profile_service_instance = None
+
+
+@app.before_request
+def before_request():
+  global user_profile_service_instance
+  global optimizely_instance
+
+  user_profile_service_instance = None
+  optimizely_instance = None
+
+  request.payload = request.get_json()
+  user_profile_service_instance = request.payload.get('user_profile_service')
+  if user_profile_service_instance:
+    ups_class = getattr(user_profile_service, request.payload.get('user_profile_service'))
+    user_profile_service_instance = ups_class(request.payload.get('user_profiles'))
+
+  optimizely_instance = optimizely.Optimizely(datafile_content, logger=logger.SimpleLogger(), user_profile_service=user_profile_service_instance)
 
 
 @app.route('/activate', methods=['POST'])
@@ -19,8 +38,10 @@ def activate():
   experiment_key = payload.get('experiment_key')
   user_id = payload.get('user_id')
   attributes = payload.get('attributes')
-  variation = opt_obj.activate(experiment_key, user_id, attributes=attributes)
-  return json.dumps({'result': variation}), 200, {'content-type': 'application/json'}
+  variation = optimizely_instance.activate(experiment_key, user_id, attributes=attributes)
+  user_profiles = user_profile_service_instance.user_profiles.values() if user_profile_service_instance else {}
+  return json.dumps({'result': variation, 'user_profiles': user_profiles}), 200, {'content-type': 'application/json'}
+
 
 @app.route('/get_variation', methods=['POST'])
 def get_variation():
@@ -28,8 +49,10 @@ def get_variation():
   experiment_key = payload.get('experiment_key')
   user_id = payload.get('user_id')
   attributes = payload.get('attributes')
-  variation = opt_obj.get_variation(experiment_key, user_id, attributes=attributes)
-  return json.dumps({'result': variation}), 200, {'content-type': 'application/json'}
+  variation = optimizely_instance.get_variation(experiment_key, user_id, attributes=attributes)
+  user_profiles = user_profile_service_instance.user_profiles.values() if user_profile_service_instance else {}
+  return json.dumps({'result': variation, 'user_profiles': user_profiles}), 200, {'content-type': 'application/json'}
+
 
 @app.route('/track', methods=['POST'])
 def track():
@@ -38,8 +61,9 @@ def track():
   user_id = payload.get('user_id')
   attributes = payload.get('attributes')
   event_value = payload.get('event_value')
-  result = opt_obj.track(event_key, user_id, attributes, event_value)
-  return json.dumps({'result': result}), 200, {'content-type': 'application/json'}
+  result = optimizely_instance.track(event_key, user_id, attributes, event_value)
+  user_profiles = user_profile_service_instance.user_profiles.values() if user_profile_service_instance else {}
+  return json.dumps({'result': result, 'user_profiles': user_profiles}), 200, {'content-type': 'application/json'}
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=3000)
