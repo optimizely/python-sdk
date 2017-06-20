315275dd9c38d524a1da031c384b410af71a19e9
Matt Auerbach <tennisfan0526@gmail.com>
2017-06-20 12:50:29 -0700

    Introduce v3 batch event endpoint to Python SDK (#61)

--- optimizely/event_builder.py
+++ optimizely/event_builder.py
@@ -12,6 +12,7 @@
 # limitations under the License.
 
 import time
+import uuid
 from abc import abstractmethod
 from abc import abstractproperty
 
@@ -272,6 +273,211 @@ class EventBuilder(BaseEventBuilder):
     self._add_common_params(user_id, attributes)
     self._add_required_params_for_conversion(event_key, event_tags, decisions)
     return Event(self.CONVERSION_ENDPOINT,
+                 self.params,
+                 http_verb=self.HTTP_VERB,
+                 headers=self.HTTP_HEADERS)
+
+
+class EventBuilderV3(BaseEventBuilder):
+  """ Class which encapsulates methods to build events for tracking
+  impressions and conversions using the new V3 event API (batch). """
+
+  EVENTS_URL = 'https://logx.optimizely.com/v1/events'
+  HTTP_VERB = 'POST'
+  HTTP_HEADERS = {'Content-Type': 'application/json'}
+
+  class EventParams(object):
+    ACCOUNT_ID = 'account_id'
+    PROJECT_ID = 'project_id'
+    EXPERIMENT_ID = 'experiment_id'
+    CAMPAIGN_ID = 'campaign_id'
+    VARIATION_ID = 'variation_id'
+    END_USER_ID = 'visitor_id'
+    EVENTS = 'events'
+    EVENT_ID = 'entity_id'
+    ATTRIBUTES = 'attributes'
+    DECISIONS = 'decisions'
+    REVISION = 'revision'
+    TIME = 'timestamp'
+    KEY = 'key'
+    TAGS = 'tags'
+    UUID = 'uuid'
+    USERS = 'visitors'
+    SNAPSHOTS = 'snapshots'
+    SOURCE_SDK_TYPE = 'client_name'
+    SOURCE_SDK_VERSION = 'client_version'
+    CUSTOM = 'custom'
+
+  def _add_attributes(self, attributes):
+    """ Add attribute(s) information to the event.
+
+    Args:
+      attributes: Dict representing user attributes and values which need to be recorded.
+    """
+
+    if not attributes:
+      return
+
+    visitor = self.params[self.EventParams.USERS][0]
+    visitor[self.EventParams.ATTRIBUTES] = []
+
+    for attribute_key in attributes.keys():
+      attribute_value = attributes.get(attribute_key)
+      # Omit falsy attribute values
+      if attribute_value:
+        attribute = self.config.get_attribute(attribute_key)
+        if attribute:
+          visitor[self.EventParams.ATTRIBUTES].append({
+            self.EventParams.EVENT_ID: attribute.id,
+            'key': attribute_key,
+            'type': self.EventParams.CUSTOM,
+            'value': attribute_value,
+          })
+
+  def _add_source(self):
+    """ Add source information to the event. """
+
+    self.params[self.EventParams.SOURCE_SDK_TYPE] = 'python-sdk'
+    self.params[self.EventParams.SOURCE_SDK_VERSION] = version.__version__
+
+  def _add_revision(self):
+    """ Add datafile revision information to the event. """
+    self.params[self.EventParams.REVISION] = self.config.get_revision()
+
+  def _add_time(self):
+    """ Add time information to the event. """
+
+    self.params[self.EventParams.TIME] = int(round(time.time() * 1000))
+
+  def _add_visitor(self, user_id):
+    """ Add user to the event """
+
+    self.params[self.EventParams.USERS] = []
+    # Add a single visitor
+    visitor = {}
+    visitor[self.EventParams.END_USER_ID] = user_id
+    visitor[self.EventParams.SNAPSHOTS] = []
+    self.params[self.EventParams.USERS].append(visitor)
+
+  def _add_common_params(self, user_id, attributes):
+    """ Add params which are used same in both conversion and impression events.
+
+    Args:
+      user_id: ID for user.
+      attributes: Dict representing user attributes and values which need to be recorded.
+    """
+    self._add_project_id()
+    self._add_account_id()
+    self._add_visitor(user_id)
+    self._add_attributes(attributes)
+    self._add_source()
+    self._add_revision()
+
+  def _add_required_params_for_impression(self, experiment, variation_id):
+    """ Add parameters that are required for the impression event to register.
+
+    Args:
+      experiment: Experiment for which impression needs to be recorded.
+      variation_id: ID for variation which would be presented to user.
+    """
+    snapshot = {}
+
+    snapshot[self.EventParams.DECISIONS] = [{
+      self.EventParams.EXPERIMENT_ID: experiment.id,
+      self.EventParams.VARIATION_ID: variation_id,
+      self.EventParams.CAMPAIGN_ID: experiment.layerId
+    }]
+
+    snapshot[self.EventParams.EVENTS] = [{
+      self.EventParams.EVENT_ID: experiment.layerId,
+      self.EventParams.TIME: int(round(time.time() * 1000)),
+      self.EventParams.KEY: 'campaign_activated',
+      self.EventParams.UUID: str(uuid.uuid4())
+    }]
+
+    visitor = self.params[self.EventParams.USERS][0]
+    visitor[self.EventParams.SNAPSHOTS].append(snapshot)
+
+  def _add_required_params_for_conversion(self, event_key, event_tags, decisions):
+    """ Add parameters that are required for the conversion event to register.
+
+    Args:
+      event_key: Key representing the event which needs to be recorded.
+      event_tags: Dict representing metadata associated with the event.
+      decisions: List of tuples representing valid experiments IDs and variation IDs.
+    """
+
+    visitor = self.params[self.EventParams.USERS][0]
+
+    for experiment_id, variation_id in decisions:
+      snapshot = {}
+      experiment = self.config.get_experiment_from_id(experiment_id)
+
+      if variation_id:
+        snapshot[self.EventParams.DECISIONS] = [{
+          self.EventParams.EXPERIMENT_ID: experiment_id,
+          self.EventParams.VARIATION_ID: variation_id,
+          self.EventParams.CAMPAIGN_ID: experiment.layerId
+        }]
+
+        event_dict = {
+          self.EventParams.EVENT_ID: self.config.get_event(event_key).id,
+          self.EventParams.TIME: int(round(time.time() * 1000)),
+          self.EventParams.KEY: event_key,
+          self.EventParams.UUID: str(uuid.uuid4())
+        }
+
+        if event_tags:
+          event_value = event_tag_utils.get_revenue_value(event_tags)
+          if event_value is not None:
+            event_dict['revenue'] = event_value
+
+          if len(event_tags) > 0:
+            event_dict[self.EventParams.TAGS] = event_tags
+
+        snapshot[self.EventParams.EVENTS] = [event_dict]
+        visitor[self.EventParams.SNAPSHOTS].append(snapshot)
+
+  def create_impression_event(self, experiment, variation_id, user_id, attributes):
+    """ Create impression Event to be sent to the logging endpoint.
+
+    Args:
+      experiment: Experiment for which impression needs to be recorded.
+      variation_id: ID for variation which would be presented to user.
+      user_id: ID for user.
+      attributes: Dict representing user attributes and values which need to be recorded.
+
+    Returns:
+      Event object encapsulating the impression event.
+    """
+
+    self.params = {}
+    self._add_common_params(user_id, attributes)
+    self._add_required_params_for_impression(experiment, variation_id)
+
+    return Event(self.EVENTS_URL,
+                 self.params,
+                 http_verb=self.HTTP_VERB,
+                 headers=self.HTTP_HEADERS)
+
+  def create_conversion_event(self, event_key, user_id, attributes, event_tags, decisions):
+    """ Create conversion Event to be sent to the logging endpoint.
+
+    Args:
+      event_key: Key representing the event which needs to be recorded.
+      user_id: ID for user.
+      attributes: Dict representing user attributes and values.
+      event_tags: Dict representing metadata associated with the event.
+      decisions: List of tuples representing experiments IDs and variation IDs.
+
+    Returns:
+      Event object encapsulating the conversion event.
+    """
+
+    self.params = {}
+    self._add_common_params(user_id, attributes)
+    self._add_required_params_for_conversion(event_key, event_tags, decisions)
+    return Event(self.EVENTS_URL,
                  self.params,
                  http_verb=self.HTTP_VERB,
                  headers=self.HTTP_HEADERS)
--- tests/base.py
+++ tests/base.py
@@ -14,7 +14,12 @@
 import json
 import unittest
 
+
+from optimizely import error_handler
+from optimizely import event_builder
+from optimizely import logger
 from optimizely import optimizely
+from optimizely import project_config
 
 
 class BaseTest(unittest.TestCase):
@@ -306,4 +311,228 @@ class BaseTest(unittest.TestCase):
     }
 
     self.optimizely = optimizely.Optimizely(json.dumps(self.config_dict))
+    self.project_config = self.optimizely.config
+
+
+class BaseTestV3(unittest.TestCase):
+
+  def setUp(self):
+    self.config_dict = {
+      'revision': '42',
+      'version': '2',
+      'events': [{
+        'key': 'test_event',
+        'experimentIds': ['111127'],
+        'id': '111095'
+      }, {
+        'key': 'Total Revenue',
+        'experimentIds': ['111127'],
+        'id': '111096'
+      }],
+      'experiments': [{
+        'key': 'test_experiment',
+        'status': 'Running',
+        'forcedVariations': {
+          'user_1': 'control',
+          'user_2': 'control'
+        },
+        'layerId': '111182',
+        'audienceIds': ['11154'],
+        'trafficAllocation': [{
+          'entityId': '111128',
+          'endOfRange': 4000
+        }, {
+          'entityId': '',
+          'endOfRange': 5000
+        }, {
+          'entityId': '111129',
+          'endOfRange': 9000
+        }],
+        'id': '111127',
+        'variations': [{
+          'key': 'control',
+          'id': '111128'
+        }, {
+          'key': 'variation',
+          'id': '111129'
+        }]
+      }],
+      'groups': [{
+        'id': '19228',
+        'policy': 'random',
+        'experiments': [{
+          'id': '32222',
+          'key': 'group_exp_1',
+          'status': 'Running',
+          'audienceIds': [],
+          'layerId': '111183',
+          'variations': [{
+            'key': 'group_exp_1_control',
+            'id': '28901'
+          }, {
+            'key': 'group_exp_1_variation',
+            'id': '28902'
+          }],
+          'forcedVariations': {
+            'user_1': 'group_exp_1_control',
+            'user_2': 'group_exp_1_control'
+          },
+          'trafficAllocation': [{
+            'entityId': '28901',
+            'endOfRange': 3000
+          }, {
+            'entityId': '28902',
+            'endOfRange': 9000
+          }]
+        }, {
+          'id': '32223',
+          'key': 'group_exp_2',
+          'status': 'Running',
+          'audienceIds': [],
+          'layerId': '111184',
+          'variations': [{
+            'key': 'group_exp_2_control',
+            'id': '28905'
+          }, {
+            'key': 'group_exp_2_variation',
+            'id': '28906'
+          }],
+          'forcedVariations': {
+            'user_1': 'group_exp_2_control',
+            'user_2': 'group_exp_2_control'
+          },
+          'trafficAllocation': [{
+            'entityId': '28905',
+            'endOfRange': 8000
+          }, {
+            'entityId': '28906',
+            'endOfRange': 10000
+          }]
+        }],
+        'trafficAllocation': [{
+          'entityId': '32222',
+          "endOfRange": 3000
+        }, {
+          'entityId': '32223',
+          'endOfRange': 7500
+        }]
+      }],
+      'accountId': '12001',
+      'attributes': [{
+        'key': 'test_attribute',
+        'id': '111094'
+      }],
+      'audiences': [{
+        'name': 'Test attribute users',
+        'conditions': '["and", ["or", ["or", '
+                      '{"name": "test_attribute", "type": "custom_attribute", "value": "test_value"}]]]',
+        'id': '11154'
+      }],
+      'projectId': '111001'
+    }
+
+    # datafile version 4
+    self.config_dict_with_features = {
+      'revision': '1',
+      'accountId': '12001',
+      'projectId': '111111',
+      'version': '4',
+      'events': [{
+        'key': 'test_event',
+        'experimentIds': ['111127'],
+        'id': '111095'
+      }],
+      'experiments': [{
+        'key': 'test_experiment',
+        'status': 'Running',
+        'forcedVariations': {},
+        'layerId': '111182',
+        'audienceIds': [],
+        'trafficAllocation': [{
+          'entityId': '111128',
+          'endOfRange': 5000
+        }, {
+          'entityId': '111129',
+          'endOfRange': 9000
+        }],
+        'id': '111127',
+        'variations': [{
+          'key': 'control',
+          'id': '111128',
+          'variables': [{
+            'id': '127', 'value': 'false'
+          }, {
+            'id': '128', 'value': 'prod'
+          }]
+        }, {
+          'key': 'variation',
+          'id': '111129'
+        }]
+      }],
+      'groups': [],
+      'attributes': [{
+        'key': 'test_attribute',
+        'id': '111094'
+      }],
+      'audiences': [{
+        'name': 'Test attribute users',
+        'conditions': '["and", ["or", ["or", '
+                      '{"name": "test_attribute", "type": "custom_attribute", "value": "test_value"}]]]',
+        'id': '11154'
+      }],
+      'layers': [{
+        'id': '211111',
+        'policy': 'ordered',
+        'experiments': [{
+          'key': 'test_rollout_exp_1',
+          'status': 'Running',
+          'forcedVariations': {},
+          'layerId': '211111',
+          'audienceIds': ['11154'],
+          'trafficAllocation': [{
+            'entityId': '211128',
+            'endOfRange': 5000
+          }, {
+            'entityId': '211129',
+            'endOfRange': 9000
+          }],
+          'id': '211127',
+          'variations': [{
+            'key': 'control',
+            'id': '211128'
+          }, {
+            'key': 'variation',
+            'id': '211129'
+          }]
+        }]
+      }],
+      'features': [{
+        'id': '91111',
+        'key': 'test_feature_1',
+        'experimentIds': ['111127'],
+        'layerId': '',
+        'variables': [{
+            'id': '127',
+            'key': 'is_working',
+            'defaultValue': 'true',
+            'type': 'boolean',
+          }, {
+            'id': '128',
+            'key': 'environment',
+            'defaultValue': 'devel',
+            'type': 'string',
+          }]
+      }, {
+        'id': '91112',
+        'key': 'test_feature_2',
+        'experimentIds': [],
+        'layerId': '211111',
+        'variables': [],
+      }]
+    }
+
+    self.optimizely = optimizely.Optimizely(json.dumps(self.config_dict))
+    self.config = project_config.ProjectConfig(json.dumps(self.config_dict),
+                                               logger.SimpleLogger(), error_handler.NoOpErrorHandler())
+    self.optimizely.event_builder = event_builder.EventBuilderV3(self.config)
     self.project_config = self.optimizely.config
