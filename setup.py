736de4e69273aa7b0779aa54a9f7c0ae92e527e2
aliabbasrizvi <ali@optimizely.com>
2018-01-05 10:39:08 -0800

    Updating to drop OS support at the moment

--- setup.py
+++ setup.py
@@ -31,7 +31,6 @@ setup(
       'Intended Audience :: Developers',
       'Operating System :: OS Independent',
       'Programming Language :: Python',
-      'Programming Language :: Python :: 2.6',
       'Programming Language :: Python :: 2.7',
       'Programming Language :: Python :: 3.4',
       'Programming Language :: Python :: 3.5',
