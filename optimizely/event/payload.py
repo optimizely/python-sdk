96b4d557c04d48a98f5b91ebbb554e33c33c1809
msohailhussain <mirza.sohailhussain@gmail.com>
2019-09-11 22:29:37 -0700

    feat(eventprocessor): Datamodel for event processor (#192)

---
+++ optimizely/event/payload.py
@@ -0,0 +1,94 @@
+# Copyright 2019 Optimizely
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import json
+
+
+class EventBatch(object):
+  """ Class respresenting Event Batch. """
+
+  def __init__(self, account_id, project_id, revision, client_name, client_version,
+               anonymize_ip, enrich_decisions=True, visitors=None):
+    self.account_id = account_id
+    self.project_id = project_id
+    self.revision = revision
+    self.client_name = client_name
+    self.client_version = client_version
+    self.anonymize_ip = anonymize_ip
+    self.enrich_decisions = enrich_decisions
+    self.visitors = visitors or []
+
+  def __eq__(self, other):
+    batch_obj = json.loads(json.dumps(self.__dict__, default=lambda o: o.__dict__),
+                            object_pairs_hook=self._dict_clean)
+    return batch_obj == other
+
+  def _dict_clean(self, obj):
+    """ Helper method to remove keys from dictionary with None values. """
+
+    result = {}
+    for k, v in obj:
+      if v is None and k in ['revenue', 'value', 'tags', 'decisions']:
+        continue
+      else:
+        result[k] = v
+    return result
+
+
+class Decision(object):
+  """ Class respresenting Decision. """
+
+  def __init__(self, campaign_id, experiment_id, variation_id):
+    self.campaign_id = campaign_id
+    self.experiment_id = experiment_id
+    self.variation_id = variation_id
+
+
+class Snapshot(object):
+  """ Class representing Snapshot. """
+
+  def __init__(self, events, decisions=None):
+    self.events = events
+    self.decisions = decisions
+
+
+class SnapshotEvent(object):
+  """ Class representing Snapshot Event. """
+
+  def __init__(self, entity_id, uuid, key, timestamp, revenue=None, value=None, tags=None):
+    self.entity_id = entity_id
+    self.uuid = uuid
+    self.key = key
+    self.timestamp = timestamp
+    self.revenue = revenue
+    self.value = value
+    self.tags = tags
+
+
+class Visitor(object):
+  """ Class representing Visitor. """
+
+  def __init__(self, snapshots, attributes, visitor_id):
+    self.snapshots = snapshots
+    self.attributes = attributes
+    self.visitor_id = visitor_id
+
+
+class VisitorAttribute(object):
+  """ Class representing Visitor Attribute. """
+
+  def __init__(self, entity_id, key, attribute_type, value):
+    self.entity_id = entity_id
+    self.key = key
+    self.type = attribute_type
+    self.value = value
---
+++ optimizely/event/user_event.py
@@ -0,0 +1,67 @@
+# Copyright 2019 Optimizely
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import time
+import uuid
+
+from optimizely import version
+
+CLIENT_NAME = 'python-sdk'
+
+
+class UserEvent(object):
+  """ Class respresenting User Event. """
+
+  def __init__(self, event_context, user_id, visitor_attributes, bot_filtering=None):
+    self.event_context = event_context
+    self.user_id = user_id
+    self.visitor_attributes = visitor_attributes
+    self.bot_filtering = bot_filtering
+    self.uuid = self._get_uuid()
+    self.timestamp = self._get_time()
+
+  def _get_time(self):
+    return int(round(time.time() * 1000))
+
+  def _get_uuid(self):
+    return str(uuid.uuid4())
+
+
+class ImpressionEvent(UserEvent):
+  """ Class representing Impression Event. """
+
+  def __init__(self, event_context, user_id, experiment, visitor_attributes, variation, bot_filtering=None):
+    super(ImpressionEvent, self).__init__(event_context, user_id, visitor_attributes, bot_filtering)
+    self.experiment = experiment
+    self.variation = variation
+
+
+class ConversionEvent(UserEvent):
+  """ Class representing Conversion Event. """
+
+  def __init__(self, event_context, event, user_id, visitor_attributes, event_tags, bot_filtering=None):
+    super(ConversionEvent, self).__init__(event_context, user_id, visitor_attributes, bot_filtering)
+    self.event = event
+    self.event_tags = event_tags
+
+
+class EventContext(object):
+  """ Class respresenting User Event Context. """
+
+  def __init__(self, account_id, project_id, revision, anonymize_ip):
+    self.account_id = account_id
+    self.project_id = project_id
+    self.revision = revision
+    self.client_name = CLIENT_NAME
+    self.client_version = version.__version__
+    self.anonymize_ip = anonymize_ip
