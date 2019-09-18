daafa4be864bd6e8ee01925c2c35299d9a4199b1
Owais <oakbani@folio3.com>
2019-09-18 10:24:10 +0500

    make update interval small

--- tests/test_config_manager.py
+++ tests/test_config_manager.py
@@ -282,11 +282,12 @@ class PollingConfigManagerTest(base.BaseTest):
         blocking_queue = queue.Queue()
         with mock.patch('optimizely.config_manager.PollingConfigManager.fetch_datafile',
                         side_effect=lambda: blocking_queue.put_nowait('fetch_datafile called')) as mock_fetch_datafile:
-            project_config_manager = config_manager.PollingConfigManager(sdk_key='some_key')
+            project_config_manager = config_manager.PollingConfigManager(
+                sdk_key='some_key', update_interval=1)
 
         self.assertTrue(project_config_manager.is_running)
-        # Wait for 10 seconds before asserting mock
+        # Wait for 5 seconds before asserting mock
         try:
-            blocking_queue.get(True, 10)
+            blocking_queue.get(True, 5)
         except queue.Empty:
             mock_fetch_datafile.assert_called_with()
