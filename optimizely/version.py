a6accdf2f111e9161e3785e02c8f1dbcc91383b3
aliabbasrizvi <ali@optimizely.com>
2019-10-22 10:51:32 -0700

    chore: Preparing for 3.3.0 release

--- optimizely/version.py
+++ optimizely/version.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (3, 2, 0)
+version_info = (3, 3, 0)
 __version__ = '.'.join(str(v) for v in version_info)
