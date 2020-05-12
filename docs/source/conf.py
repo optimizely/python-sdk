3504665a674cfb65e9572d02f58af1648aefc7e0
aliabbasrizvi <ali@optimizely.com>
2020-05-12 10:34:58 -0700

    Skipping lint error

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('../..'))
 
-from optimizely.version import __version__
+from optimizely.version import __version__      # noqa: E402
 
 # -- Project information -----------------------------------------------------
 
