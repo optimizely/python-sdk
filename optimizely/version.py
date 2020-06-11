7f5bc47499c0b49378d3fa9c39791b382196426c
Ali Abbas Rizvi <aliabbasrizvi@users.noreply.github.com>
2020-06-11 11:07:30 -0700

    chore(release): Update version

--- optimizely/version.py
+++ optimizely/version.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (3, 4, 1)
+version_info = (3, 4, 2)
 __version__ = '.'.join(str(v) for v in version_info)
