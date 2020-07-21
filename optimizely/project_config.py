6357c048d5c92a2034986236db467f7563b77370
aliabbasrizvi <ali@optimizely.com>
2020-07-21 10:16:05 -0700

    Setting as unicode

--- optimizely/project_config.py
+++ optimizely/project_config.py
@@ -40,7 +40,7 @@ class ProjectConfig(object):
         """
 
         config = json.loads(datafile)
-        self._datafile = datafile.encode('utf-8')
+        self._datafile = u'{}'.format(datafile)
         self.logger = logger
         self.error_handler = error_handler
         self.version = config.get('version')
