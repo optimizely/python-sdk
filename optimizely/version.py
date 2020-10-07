cc8147651d0ef834674d73ae39fb14968f08a8c9
Pawel Szczodruch <pawel.szczodruch@optimizely.com>
2020-10-06 17:30:44 -0700

    added changes

--- optimizely/version.py
+++ optimizely/version.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (3, 5, 2)
+version_info = (3, 6, 0)
 __version__ = '.'.join(str(v) for v in version_info)
