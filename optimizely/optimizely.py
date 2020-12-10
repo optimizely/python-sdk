2c1f8b08a6fcd1954ecc9fc2898957855ccab06a
Tom Zurkan <thomas.zurkan@optimizely.com>
2020-12-10 10:17:36 -0800

    only capture info messages and up

--- optimizely/optimizely.py
+++ optimizely/optimizely.py
@@ -1006,7 +1006,8 @@ class Optimizely(object):
                 super(ReasonLogHandler, self).__init__()
                 self._name = "ReasonLogHandler"
                 self.reasons = {threading.current_thread().ident: []}
-                self.level = logging.DEBUG
+                # setting to info level since we don't put debug in reasons.
+                self.level = logging.INFO
                 formatter = logging.Formatter('%(levelname)-8s %(asctime)s %(filename)s:%(lineno)s:%(message)s')
                 self.setFormatter(formatter)
                 self.createLock()
--- tests/test_user_context.py
+++ tests/test_user_context.py
@@ -349,9 +349,9 @@ class UserContextTests(base.BaseTest):
         )
 
         self.assertIsNotNone(decision.reasons)
-        self.assertTrue(decision.reasons[2].find(
+        self.assertTrue(decision.reasons[0].find(
             'Audiences for experiment "test_experiment" collectively evaluated to TRUE.') is not -1)
-        self.assertTrue(decision.reasons[3].find(
+        self.assertTrue(decision.reasons[1].find(
             'User "test_user" is in variation "variation" of experiment test_experiment.') is not -1)
         # Check that impression event is NOT sent for rollout and send_flag_decisions = True
         # with disable decision event decision option
