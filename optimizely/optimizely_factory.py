1e8ef28af2d27e1f21add0e925d433200af5e558
ozayr-zaviar <uzairzaviar@gmail.com>
2021-05-10 20:49:03 +0500

    whitespaced removed

--- optimizely/optimizely_factory.py
+++ optimizely/optimizely_factory.py
@@ -138,7 +138,7 @@ class OptimizelyFactory(object):
                datafile: Optional JSON string datafile.
                event_dispatcher: Optional EventDispatcherInterface Provides a dispatch_event method which if given a URL
                                 and params sends a request to it.
-               logger: Optional LoggerInterface Provides a log method to log messages. 
+               logger: Optional LoggerInterface Provides a log method to log messages.
                        By default nothing would be logged.
                error_handler: Optional ErrorHandlerInterface which provides a handle_error method to handle exceptions.
                               By default all exceptions will be suppressed.
