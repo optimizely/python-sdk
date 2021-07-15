b08c3cba6de2c92273ee59ec67a530139617c956
Jacob Brown <jacob.brown@optimizely.com>
2021-07-15 08:05:24 -0400

    Update in user_event_factory to use experiemnt_id for impression events.

--- optimizely/event/user_event_factory.py
+++ optimizely/event/user_event_factory.py
@@ -45,12 +45,12 @@ class UserEventFactory(object):
         if not activated_experiment and rule_type is not enums.DecisionSources.ROLLOUT:
             return None
 
-        variation, experiment_key = None, None
+        variation, experiment_id = None, None
         if activated_experiment:
-            experiment_key = activated_experiment.key
+            experiment_id = activated_experiment.id
 
-        if variation_id and experiment_key:
-            variation = project_config.get_variation_from_id(experiment_key, variation_id)
+        if variation_id and experiment_id:
+            variation = project_config.get_variation_from_id_by_experiment_id(experiment_id, variation_id)
         event_context = user_event.EventContext(
             project_config.account_id, project_config.project_id, project_config.revision, project_config.anonymize_ip,
         )
