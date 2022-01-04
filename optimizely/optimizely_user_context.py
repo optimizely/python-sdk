009974030e8eaca3ed7f8090a1ae8332e73ca325
ozayr-zaviar <uzairzaviar@gmail.com>
2022-01-04 18:21:03 +0500

    headers updated

--- optimizely/optimizely_user_context.py
+++ optimizely/optimizely_user_context.py
@@ -1,4 +1,4 @@
-#    Copyright 2021, Optimizely and contributors
+#    Copyright 2021-2022, Optimizely and contributors
 #
 #    Licensed under the Apache License, Version 2.0 (the "License");
 #    you may not use this file except in compliance with the License.
