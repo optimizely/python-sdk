4c8ad4ce11a3c7be3afa97aebf08df3f99cfaa06
Ozayr <54209343+ozayr-zaviar@users.noreply.github.com>
2022-11-28 19:53:00 +0500

    Update odp_event_manager.py

--- optimizely/odp/odp_event_manager.py
+++ optimizely/odp/odp_event_manager.py
@@ -66,7 +66,7 @@ class OdpEventManager:
         self._flush_deadline: float = 0
         self.retry_count = OdpEventManagerConfig.DEFAULT_RETRY_COUNT
         self._current_batch: list[OdpEvent] = []
-        self.odp_event_timeout = Optional[int] = None
+        self.odp_event_timeout: Optional[int] = None
         """_current_batch should only be modified by the processing thread, as it is not thread safe"""
         self.thread = Thread(target=self._run, daemon=True)
         self.thread_exception = False
