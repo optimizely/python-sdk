7fa6153d898687bc616e8f1d6920106f75ca19d0
Farhan Anjum <Farhan.Anjum@optimizely.com>
2024-11-29 10:58:15 +0600

    CHANGELOG.md -> Added section for version 5.1.0  version.py -> Updated version to 5.1.0 (#441)

--- optimizely/version.py
+++ optimizely/version.py
@@ -11,5 +11,5 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-version_info = (5, 0, 1)
+version_info = (5, 1, 0)
 __version__ = '.'.join(str(v) for v in version_info)
