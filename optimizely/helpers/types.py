3a52b56aa7a50641c3aa9a976a5b0770e5b90b93
FarhanAnjum-opti <farhan.anjum@optimizely.com>
2026-03-11 22:11:58 +0600

    Format ExperimentType definition for ruff check

--- optimizely/helpers/types.py
+++ optimizely/helpers/types.py
@@ -117,7 +117,9 @@ class CmabDict(BaseEntity):
     trafficAllocation: int
 
 
-ExperimentType = Literal['a/b', 'multi_armed_bandit', 'contextual_multi_armed_bandit', 'targeted_delivery', 'feature_rollout']
+ExperimentType = Literal[
+    'a/b', 'multi_armed_bandit', 'contextual_multi_armed_bandit', 'targeted_delivery', 'feature_rollout'
+]
 
 HoldoutStatus = Literal['Draft', 'Running', 'Concluded', 'Archived']
 
